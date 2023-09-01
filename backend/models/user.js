1a7a6574729a1c4bc0e7df52f2f641e62cdcff09
rmhayden <robertmorsehayden@gmail.com>
2023-09-01 16:33:09 -0400

    added salt and bcrypt, but not yet token

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 6;
 
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
@@ -20,7 +23,23 @@ const userSchema = new mongoose.Schema({
         ref: "Case"
     }]
 }, {
-    timestamps: true
+    timestamps: true,
+    toJSON: {
+      transform: function(doc, ret){
+        delete ret.password;
+        return ret;
+      }
+    }
 })
+
+userSchema.pre('save', async function (next) {
+
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+
+});
+
+
 
 module.exports = mongoose.model('User', userSchema)
