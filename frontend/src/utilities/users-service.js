ccda6364cc68dbb5eff978a9ac62f3967af90d4a
william-hu-codes <william.hu307@gmail.com>
2023-09-03 10:39:09 -0400

    fix login functionality

--- frontend/src/utilities/users-service.js
+++ frontend/src/utilities/users-service.js
@@ -9,18 +9,22 @@ export async function signUp(userData) {
 }
 
 export function getToken() {
-    // getItem will return null if the key does not exist
-    const token = localStorage.getItem("token")
-    if (!token) return null
-    const payload = JSON.parse(atob(token.split(".")[1]))
-    if (payload.exp * 1000 > Date.now() ) {
-        localStorage.removeItem("token")
-        return null
+    // getItem returns null if there's no string
+    const token = localStorage.getItem('token');
+    if (!token) return null;
+    // Obtain the payload of the token
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    // A JWT's exp is expressed in seconds, not milliseconds, so convert
+    if (payload.exp < Date.now() / 1000) {
+      // Token has expired - remove it from localStorage
+      localStorage.removeItem('token');
+      return null;
     }
-    return token
-}
-
-export function getUser() {
-    const token = getToken()
-    return token ? JSON.parse(atob(token.split(".")[1]).user) : null
-}
+    return token;
+  }
+  
+  export function getUser() {
+    const token = getToken();
+    // If there's a token, return the user in the payload, otherwise return null
+    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
+  }
