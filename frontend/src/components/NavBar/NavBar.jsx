f93eeb61709742ed33bea21b4fbe7a3cc69678f3
william-hu-codes <william.hu307@gmail.com>
2023-09-21 09:44:05 -0400

    update nav

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import * as userService from "../../utilities/users-service"
+import "./NavBar.css"
 
 
 export default function NavBar ({ user, setUser }) {
@@ -11,11 +12,27 @@ export default function NavBar ({ user, setUser }) {
     return (
 
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/:userid">Profile</Link>
-            {/* <span>Welcome, {user.name}</span> */}
-            <Link to="" onClick={handleLogOut}>Log Out</Link>
+             <Link className="link" to="/aki/about">
+                <button className="nav-link"><h4>HOME</h4></button>
+            </Link>
+            <Link className="link" to="/aki">
+                <button className="nav-link"><h4>MY CASES</h4></button>
+            </Link>
+            { user !== null ? 
+                <>
+                    <Link className="link" to="/aki/user">
+                        <button className="nav-link profile-button"><span><h4>PROFILE: </h4><p> {user?.name}</p></span></button>
+                    </Link>
+                    {/* <span><strong>Welcome, {user?.name}</strong> </span> */}
+                    <Link className="link logout" to="/aki" onClick={handleLogOut}>
+                        <button className="nav-link"><h4>LOG OUT</h4></button>
+                    </Link>
+                </>
+                :
+                null 
+            }
 
         </nav>
     );
 }
+
